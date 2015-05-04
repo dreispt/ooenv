3e6f65f19eeedb6349f55af197b5c47c7b27de94
Daniel Reis <dgreis@sapo.pt>
2015-05-04 10:35:05 +0100

    Add verbosity options to the get command

--- odoo-env/cli/get.py
+++ odoo-env/cli/get.py
@@ -9,10 +9,16 @@ from openerp.modules.module import get_modules
 from . import utils
 
 
+def print_if(flag, text):
+    if flag:
+        print(text)
+
+
 class Get(Command):
     """ Get Odoo modules """
 
-    def get_module(self, module, env_root, exclude=None):
+    def get_module(self, module, env_root, exclude=None,
+                   verbose=False, quiet=False):
         """ Download and activate module and dependencies """
         # Modules already visited are skipped
         if exclude and module in exclude:
@@ -21,12 +27,13 @@ class Get(Command):
         exclude.append(module)
         # Modules already in the path are skipped
         if module in get_modules():
-            print('. %s is available from addons path.' % module)
+            print_if(verbose, '. %s is available from addons path.' % module)
             return True
         if module in os.listdir(env_root):
             # Modules in the env root are already active
             module_path = os.path.join(env_root, module)
-            print('. %s already active (at %s)' % (module, module_path))
+            print_if(verbose, '. %s already active (at %s)' %
+                     (module, module_path))
 
         else:
             # Modules in the local cache are available to activate
@@ -36,7 +43,8 @@ class Get(Command):
                 # Modules not in the local cache are downloaded
                 index = utils.indexed_modules(path)
                 if module not in index:
-                    print('! %s was not found!' % module)
+                    print_if(not quiet, '! %s was not found in the index!' %
+                             module)
                     return False
                 utils.download_repo(path, index[module])
                 module_path = utils.crawl_modules(path).get(module)
@@ -48,7 +56,8 @@ class Get(Command):
         if module not in os.listdir(env_root):
             target_path = os.path.join(env_root, module)
             os.symlink(module_path, target_path)
-            print('+ %s activated (from %s)' % (module, module_path))
+            print_if(not quiet, '+ %s activated (from %s)' %
+                     (module, module_path))
 
         # Get dependencies
         try:
@@ -57,14 +66,16 @@ class Get(Command):
             manifest = {}
         depends = manifest.get('depends')
         for m in depends:
-            if not self.get_module(m, env_root, exclude=exclude):
+            if not self.get_module(m, env_root, exclude=exclude,
+                                   verbose=verbose, quiet=quiet):
                 return False
         return True
 
-    def get_modules(self, modules, env_root, exclude=None):
+    def get_modules(self, modules, env_root, exclude=None,
+                    verbose=None, quiet=None):
         exclude = exclude or ['base']
         for m in modules:
-            self.get_module(m, env_root, exclude)
+            self.get_module(m, env_root, exclude, verbose=verbose, quiet=quiet)
         return True
 
     def run(self, cmdargs):
@@ -83,5 +94,6 @@ class Get(Command):
         args = parser.parse_args(args=cmdargs)
 
         env_root = os.getcwd()
-        self.get_modules(args.modules, env_root)
+        self.get_modules(args.modules, env_root,
+                         verbose=args.verbose, quiet=args.quiet)
         print('Done.')
